9e70e9cf4af7d0f5c54122ab62168a1736ce78cf
Miroslavetsh <dfgshte@gmail.com>
2021-06-25 10:09:01 +0300

    Fix structure of the project, move commponents out of pages folder

--- components/ProsComment/ProsComment.jsx
+++ components/ProsComment/ProsComment.jsx
@@ -4,15 +4,15 @@ import Image from 'next/image'
 import styles from './ProsComment.module.scss'
 
 const ProsComment = ({ pro }) => {
-    const { src, height, width } = pro.img
+    const { src, height, width } = pro?.img
 
     return (
         <div className={styles.card}>
             <div className={styles.image}>
                 <Image src={src} width={width} height={height} />
             </div>
-            <div className={styles.name}>{pro.name}</div>
-            <div className={styles.description}>{pro.description}</div>
+            <div className={styles.name}>{pro?.name}</div>
+            <div className={styles.description}>{pro?.description}</div>
         </div>
     )
 }
