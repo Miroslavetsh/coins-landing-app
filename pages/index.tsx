5a511b5897f786e60d87720cfc54ae1570243757
Miroslavetsh <dfgshte@gmail.com>
2021-10-15 13:40:28 +0300

    Work with SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,16 @@ const Index: React.FC = (): JSX.Element => {
   return (
     <div className={styles.page}>
       <Head>
-        <title>Test Task</title>
-        <meta name='description' content='Test task for Andy' />
+        <title>Coin App</title>
+        <meta property='og:title' content="Miroslavetsh's coin app" />
+        <meta
+          property='og:description'
+          content='App, where you can track currencies with its own cources. Now availiable bitcoin and ethereum, but we have a big plans... :)'
+        />
+        <meta
+          property='og:image'
+          content='https://i.pinimg.com/originals/7f/e2/33/7fe233c3361ce8b81e5b17d9e334d321.jpg'
+        />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
